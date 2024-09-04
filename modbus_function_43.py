c3f7af79ca2ba03a22a5b0991f1b5ca84d24e494
btaub <beau.taub@gmail.com>
2024-09-04 12:16:52 -0400

    rewrite of function 43 script

--- modbus_function_43.py
+++ modbus_function_43.py
@@ -1,43 +1,54 @@
-
-#!/usr/bin/env python
+#! /usr/bin/env python3
 
 #---------------------------------------------------------------------------#
 # if the PLC supports it, data from function_43 will be returned, e.g.:
 #
-# [+] raw_response:
-# '\x0e\x01\x81\x00\x00\x06\x00\x14Schneider Electric  \x01\rBMX P34 20302\x02\x05v3.01'
+# Schneider Electric TM221CE40TV1.1,10.100.100.100
+#
+# if verbose is used:
+#
+# +-----------------------------------------------+
+# | decoded_response | encoded_response  | target |
+# +-----------------------------------------------+
+# Schneider Electric TM221CE40TV1.1,b'\x0e\x01\x81\x00\x00\t\x00\x12Schneider Electric\x01\nTM221CE40TV\x02\x04V1.1',10.100.100.100
 #
 #---------------------------------------------------------------------------#
-
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
-import logging
+from pymodbus.client import ModbusTcpClient as ModbusClient
+from pymodbus.mei_message import *
+from pymodbus import exceptions
 import sys
 import argparse
-from pymodbus.mei_message import *
-from pymodbus.transaction import ModbusRtuFramer as ModbusFramer
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--rhost" ,help="aatest")
-parser.add_argument("--rport", type=int)
+parser.add_argument("--rport", type=int, default=502)
+parser.add_argument("--uid", type=int, default=0)
+parser.add_argument("-v","--verbose",action="store_true",default=False)
 args = parser.parse_args()
-parser.set_defaults(rport=502)
 
-if args.rhost == None:
-    print(parser.print_help())
-    sys.exit()
+try:
+    with ModbusClient(args.rhost, port=args.rport) as client:
+        client.connect()
+        function_43_resp = client.execute(ReadDeviceInformationRequest(unit=args.uid))
 
-with ModbusClient(args.rhost, port=args.rport) as client:
-    logging.basicConfig()
-    log = logging.getLogger()
-    log.setLevel(logging.INFO)
-    #log.setLevel(logging.DEBUG)
+        resp = function_43_resp.encode()
+        resp = resp.replace(b'\n',b' ')
+        resp = resp.replace(b'\t',b'')
+        resp = resp.decode(errors='ignore')
 
-client.connect()
-func_43_resp = client.execute(ReadDeviceInformationRequest(unit=0))
-#print("FC: %s ") % func_43_resp.function_code
-#print("SUBFC: %s ") % func_43_resp.sub_function_code
-print("\n[+] raw_response:\n%r\n" % func_43_resp.encode())
-#print("\n[+] formatted_response: %s\n")  % (func_43_resp.encode()) # This doesn't print out correctly (something with struct fmt?)
+        if args.verbose:
 
-client.close()
+            print('\n+-----------------------------------------------+'
+                  '\n| decoded_response | encoded_response  | target |'
+                  '\n+-----------------------------------------------+'
+                  f'\n{resp},{function_43_resp.encode()},{args.rhost}\n')
+
+        else:
+            print(f'{resp},{args.rhost}')
+
+        client.close()
+
+except exceptions.ConnectionException as e:
+    print(f"\n[-] Exception: {e},{args.rhost}\n")
+
